b0996953ba7a954fbdff454746fde75794aa9122
Adham Shaheen <adhamshaheen282@gmail.com>
2026-05-23 19:26:28 +0300

    Configured the JWT authorization in Program.cs, added JWT JSON object to appsettings.json and added the authentication & authorization middleware.

--- HomeMaintenanceServiceMarketplace/Program.cs
+++ HomeMaintenanceServiceMarketplace/Program.cs
@@ -3,12 +3,42 @@ using HomeServicesPlatform.Application.Services;
 using HomeServicesPlatform.Infrastructure.Data;
 using HomeServicesPlatform.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<HomeServicesPlatform.Infrastructure.Data.AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<HomeServicesPlatform.Application.Interfaces.IAppDbContext, HomeServicesPlatform.Infrastructure.Data.AppDbContext>();
+
+
+// Add Authentication with JWT 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidAudience = builder.Configuration["JWT:Audience"],
+
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
+    };
+});
+
+builder.Services.AddAuthorization();
+
 
 // Register Repositories
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
@@ -26,6 +56,10 @@ using (var scope = app.Services.CreateScope())
     var context = services.GetRequiredService<HomeServicesPlatform.Infrastructure.Data.AppDbContext>();
     await HomeServicesPlatform.Infrastructure.Seed.DbSeeder.SeedData(context);
 }
+
+// Add Authentication and Authorization middleware
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
 
