9ebb4db5dc9a79bed447c4ccf41f941180a48b3f
Adham Shaheen <adhamshaheen282@gmail.com>
2026-05-24 21:28:23 +0300

    Created and implemented UserProfileDto and UpdateProfileDto

---
+++ HomeServicesPlatform.Application/DTOs/UserProfile/UpdateProfileDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeServicesPlatform.Application.DTOs.UserProfile
+{
+    public class UpdateProfileDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+    }
+}
