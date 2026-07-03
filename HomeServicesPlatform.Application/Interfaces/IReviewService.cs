6e2ca6250833b8e1bd626248af85c17dc35c9e33
Adham Shaheen <adhamshaheen282@gmail.com>
2026-07-03 14:38:33 +0300

    Created and implemented the ReviewDto and interface IReviewService

---
+++ HomeServicesPlatform.Application/Interfaces/IReviewService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HomeServicesPlatform.Application.DTOs.Review;
+
+
+namespace HomeServicesPlatform.Application.Interfaces
+{
+    public interface IReviewService
+    {
+        Task AddReviewAsync(string currentUserId, CreateReviewDto dto);
+
+        Task<IEnumerable<Domain.Models.Review>> GetProviderReviewsAsync(int providerId);
+
+        Task<IEnumerable<Domain.Models.Review>> GetMyReviewsAsync(string currentUserId);
+    }
+}
