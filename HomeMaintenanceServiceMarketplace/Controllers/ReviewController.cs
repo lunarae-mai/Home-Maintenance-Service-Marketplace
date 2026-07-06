547d7103479303cef0b2bd4fd1223565b283d6fa
nada <nada.baki54@gmail.com>
2026-07-06 17:35:28 +0300

    vv

--- HomeMaintenanceServiceMarketplace/Controllers/ReviewController.cs
+++ HomeMaintenanceServiceMarketplace/Controllers/ReviewController.cs
@@ -234,6 +234,5 @@ namespace HomeServicesPlatform.API.Controllers
         public int BookingId { get; set; }
         public int Rating { get; set; } // 1-5
         public string Comment { get; set; } = string.Empty;
->>>>>>> Stashed changes
     }
 }
